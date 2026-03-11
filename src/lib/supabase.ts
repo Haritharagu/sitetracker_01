2145fb45d0ac0248ad9ee775d649a6b867ee782c
Haritharagu <haritharagu22@gmail.com>
2026-03-11 19:10:21 +0530

    Connect Supabase backend via env variables and GitHub

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://nubuntwdkcyvhxaxzxix.supabase.co';
-const supabaseKey = 'sb_publishable_IvpxhIR7y3GVesPr2IOkGA_KA0dBGQL';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
